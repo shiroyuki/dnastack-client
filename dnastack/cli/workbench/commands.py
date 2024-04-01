50e53668bc1025808d01b99bb0664ae768fd9352
Diana-DNAstack <diana@dnastack.com>
2024-04-01 12:07:28 -0400

    Added in engines commands from alpha

--- dnastack/cli/workbench/commands.py
+++ dnastack/cli/workbench/commands.py
@@ -2,6 +2,7 @@ import click
 
 from dnastack.cli.workbench.runs_commands import runs_command_group
 from dnastack.cli.workbench.workflows_commands import workflows_command_group
+from dnastack.cli.workbench.engines_commands import engines_command_group
 
 
 @click.group('workbench')
@@ -11,3 +12,5 @@ def workbench_command_group():
 
 workbench_command_group.add_command(runs_command_group)
 workbench_command_group.add_command(workflows_command_group)
+workbench_command_group.add_command(engines_command_group)
+
