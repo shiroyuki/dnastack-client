4f0398141bacc4ba67bfc4559ae3623e05c016bb
Diana-DNAstack <diana@dnastack.com>
2024-04-01 12:48:22 -0400

    Added parameters command to engines

--- dnastack/cli/workbench/engines_commands.py
+++ dnastack/cli/workbench/engines_commands.py
@@ -17,6 +17,11 @@ def engines_command_group():
     """ Create and interact with engines """
 
 
+@click.group('parameters')
+def engine_parameters_command_group():
+    """ Create and interact with engine parametesrs """
+
+
 @command(engines_command_group,
          'list',
          specs=[
@@ -78,3 +83,61 @@ def describe_engines(context: Optional[str],
 
     described_engines = [client.get_engine(engine_id=engine) for engine in engines]
     click.echo(to_json(normalize(described_engines)))
+
+
+@command(engine_parameters_command_group,
+         'list',
+         specs=[
+             ArgumentSpec(
+                 name='namespace',
+                 arg_names=['--namespace', '-n'],
+                 help='An optional flag to define the namespace to connect to. By default, the namespace will be '
+                      'extracted from the users credentials.',
+                 as_option=True
+             ),
+             ArgumentSpec(
+                 name='engine_id',
+                 arg_names=['--engine'],
+                 help='A flag to specify the engine whose parameters should be listed',
+                 as_option=True
+             )
+         ])
+def list_engine_parameters(context: Optional[str],
+                           endpoint_id: Optional[str],
+                           namespace: Optional[str],
+                           engine_id: str = None):
+    """
+    Lists engine parameter preset values for a specified engine id
+    """
+    click.echo('list engine parameter presets')
+
+
+@command(engine_parameters_command_group,
+         'describe',
+         specs=[
+             ArgumentSpec(
+                 name='namespace',
+                 arg_names=['--namespace', '-n'],
+                 help='An optional flag to define the namespace to connect to. By default, the namespace will be '
+                      'extracted from the users credentials.',
+                 as_option=True
+             ),
+             ArgumentSpec(
+                 name='engine_id',
+                 arg_names=['--engine'],
+                 help='A flag to specify the engine whose parameter should be described',
+                 as_option=True
+             )
+         ])
+def describe_engine_parameters(context: Optional[str],
+                               endpoint_id: Optional[str],
+                               namespace: Optional[str],
+                               engine_id: str = None,
+                               preset_id: str = None):
+    """
+    Describes an engine's parameter preset value for a specified engine id and parameter id
+    """
+    click.echo('describe engine parameter presets')
+
+
+engines_command_group.add_command(engine_parameters_command_group)
