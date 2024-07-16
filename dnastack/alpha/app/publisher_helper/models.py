87c61074e6256f8d8f21ac91cae6fe56962a544b
Jim Vlasblom <jim@dnastack.com>
2024-07-16 15:14:18 -0400

    Added get_filtered_data to Explorer.py in alpha/app Also made publisher_helper/models/BaseItemInfo, TableInfo, and BlobInfo hashable.

--- dnastack/alpha/app/publisher_helper/models.py
+++ dnastack/alpha/app/publisher_helper/models.py
@@ -18,15 +18,47 @@ class BaseItemInfo(BaseModel, ABC):
     size_unit: str
     item_updated_time: datetime
 
+    def __hash__(self):
+        return hash((self.id, self.name, self.type, self.size, self.size_unit, self.item_updated_time))
+
+    def __eq__(self, other):
+        if not isinstance(other, BaseItemInfo):
+            return False
+        return (self.id, self.name, self.type, self.size, self.size_unit, self.item_updated_time) == \
+            (other.id, other.name, other.type, other.size, other.size_unit, other.item_updated_time)
+
 
 class TableInfo(BaseItemInfo):
     """ Simplified Library Item """
     json_schema: Dict[str, Any]
+    def __hash__(self):
+        return hash((super().__hash__(), frozenset(self.json_schema.items())))
+
+    def __eq__(self, other):
+        if not isinstance(other, TableInfo):
+            return False
+        return super().__eq__(other) and self.json_schema == other.json_schema
 
 
 class BlobInfo(BaseItemInfo):
     """ Simplified Library Item """
     checksums: List[Dict[str, str]] = Field(default_factory=list)
+
+
+    def __hash__(self):
+        # Convert list of dicts to a frozenset of frozensets to ensure hashability
+        checksums_frozenset = frozenset(frozenset(d.items()) for d in self.checksums)
+        return hash((super().__hash__(), checksums_frozenset))
+
+
+    def __eq__(self, other):
+        if not isinstance(other, BlobInfo):
+            return False
+        return super().__eq__(other) and self.checksums == other.checksums
+
+
+
+
 
 
 class ItemType(str, Enum):
