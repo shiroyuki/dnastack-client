862ac9d03fb8ed54bcb644401a16afc5dc72e1bb
Patrick Magee <patrickmageee@gmail.com>
2024-09-18 11:10:17 -0400

    [CU-86b262z0b] added storage add / delete commands (#25)
    * [CU-86b262z0b] added storage add / delete commands
    
    * [CU-86b262z0b] addressed PR review

--- dnastack/alpha/cli/workbench/commands.py
+++ dnastack/alpha/cli/workbench/commands.py
@@ -1,7 +1,6 @@
 import click
 
-from dnastack.alpha.cli.workbench.engines_commands import alpha_engines_command_group
-from dnastack.alpha.cli.workbench.workflows_commands import alpha_workflows_command_group
+from dnastack.alpha.cli.workbench.storage_commands import alpha_storage_command_group
 
 
 @click.group('workbench')
@@ -9,5 +8,4 @@ def alpha_workbench_command_group():
     """ Interact with Workbench """
 
 
-alpha_workbench_command_group.add_command(alpha_workflows_command_group)
-alpha_workbench_command_group.add_command(alpha_engines_command_group)
+alpha_workbench_command_group.add_command(alpha_storage_command_group)
