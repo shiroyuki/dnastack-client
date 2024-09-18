8cb7a0e885a0ea4d374930fe8f610d756b15dc55
Patrick Magee <patrick@dnastack.com>
2024-09-18 11:09:12 -0400

    [CU-86b26t7uz] added storage list and get commands

--- dnastack/alpha/client/workbench/storage/models.py
+++ dnastack/alpha/client/workbench/storage/models.py
@@ -3,6 +3,10 @@ from typing import Optional, List, Any, Literal, Union
 
 from pydantic import BaseModel
 
+from dnastack.client.workbench.models import BaseListOptions
+from dnastack.client.workbench.models import PaginatedResource
+
+
 class Provider(str, Enum):
     aws = "aws"
     gcp = "gcp"
@@ -16,6 +20,7 @@ class AwsStorageAccountCredentials(BaseModel):
     type: str = 'AWS_ACCESS_KEY'
 
 
+
 class StorageAccount(BaseModel):
     id: Optional[str]
     namespace: Optional[str]
@@ -25,3 +30,13 @@ class StorageAccount(BaseModel):
     last_updated_at: Optional[str]
     credentials: Optional[Union[AwsStorageAccountCredentials]]
 
+class StorageListOptions(BaseListOptions):
+    provider: Optional[Provider]
+    sort: Optional[str]
+
+
+class StorageListResponse(PaginatedResource):
+    accounts: List[StorageAccount]
+
+    def items(self) -> List[Any]:
+        return self.accounts
